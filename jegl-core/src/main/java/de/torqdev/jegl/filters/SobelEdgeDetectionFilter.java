6c446657a7e10ea92b2189165bd7e767c9fce898
Christopher Guckes <christopher.guckes@torq-dev.de>
2017-05-25 14:15:25 +0200

    Added two way sobel edge detection.

---
+++ jegl-core/src/main/java/de/torqdev/jegl/filters/SobelEdgeDetectionFilter.java
@@ -0,0 +1,54 @@
+package de.torqdev.jegl.filters;
+
+import de.torqdev.jegl.core.FloatImage;
+import org.kohsuke.MetaInfServices;
+
+import java.util.stream.IntStream;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.sqrt;
+
+/**
+ * @author <a href="mailto:christopher.guckes@torq-dev.de">Christopher Guckes</a>
+ * @version 1.0
+ */
+// TODO: test you moron
+@MetaInfServices
+public class SobelEdgeDetectionFilter implements ImageFilter {
+    private static final ImageFilter verticalSobel = new SobelVerticalEdgeDetectionFilter();
+    private static final ImageFilter horizontalSobel = new SobelHorizontalEdgeDetectionFilter();
+
+    @Override
+    public FloatImage processImage(FloatImage image) {
+        return applySobel(image);
+    }
+
+    private FloatImage applySobel(FloatImage image) {
+        FloatImage vertical = verticalSobel.processImage(image);
+        FloatImage horizontal = horizontalSobel.processImage(image);
+
+        return combine(vertical, horizontal);
+    }
+
+    private FloatImage combine(FloatImage vertical, FloatImage horizontal) {
+        FloatImage sobel = new FloatImage(vertical.getWidth(), vertical.getHeight(), 1);
+
+        IntStream.range(0, sobel.getHeight()).forEach(
+                y -> IntStream.range(0, sobel.getWidth()).forEach(
+                        x -> sobel.setPixel(x, y, combinePixels(vertical.getPixel(x, y), horizontal.getPixel(x, y)))
+                )
+        );
+        return sobel;
+    }
+
+    private float[] combinePixels(float[] verticalPixel, float[] horizontalPixel) {
+        float newPixel = (float) sqrt(verticalPixel[0] * verticalPixel[0] + horizontalPixel[0] * horizontalPixel[0]);
+        newPixel = normalize(newPixel);
+        return new float[] { newPixel };
+    }
+
+    private float normalize(float newPixel) {
+        return max(0F, min(1F, newPixel));
+    }
+}
