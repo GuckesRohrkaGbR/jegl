e147d3031d40596b5b22ec737f0a9726cf17e0a4
Jonas Egert <jonas@jonas-laptop.egert.local>
2017-05-26 12:00:22 +0200

    Created TransmuteColorFilter with tests

---
+++ jegl-core/src/main/java/de/torqdev/jegl/filters/TransmuteColorFilter.java
@@ -0,0 +1,32 @@
+package de.torqdev.jegl.filters;
+
+import de.torqdev.jegl.core.FloatImage;
+import org.kohsuke.MetaInfServices;
+
+/**
+ * Created by jonas on 26.05.17.
+ */
+@MetaInfServices
+public class TransmuteColorFilter implements ImageFilter {
+    @Override
+    public FloatImage processImage(FloatImage image){
+        FloatImage transmuteColor = new FloatImage(image.getWidth(), image.getHeight(),3);
+
+        for (int y=0; y < image.getHeight(); y++){
+            for (int x = 0; x < image.getWidth(); x++){
+                float [] pixel = image.getPixel(x,y);
+                transmuteColor.setPixel(x,y, transmutecolorForPixel(pixel));
+            }
+        }
+        return transmuteColor;
+    }
+
+    private float[] transmutecolorForPixel(float[] pixel) {
+        return new float[]{
+                pixel[2],
+                pixel[0],
+                pixel[1]
+        };
+    }
+
+}
---
+++ jegl-core/src/test/java/de/torqdev/jegl/filters/TransmuteColorFilterTest.java
@@ -0,0 +1,140 @@
+package de.torqdev.jegl.filters;
+
+import de.torqdev.jegl.core.FloatImage;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by jonas on 26.05.17.
+ */
+public class TransmuteColorFilterTest {
+
+    private final ImageFilter filter = new TransmuteColorFilter();
+
+    @Test
+    public void givenEmptyImage_returnsEmptyImage() throws Exception {
+        FloatImage image = new FloatImage (0,0,1);
+
+        image = filter.processImage(image);
+
+        assertThat(image.getRawData().length, is(0));
+    }
+
+    @Test
+    public void givenAnyImage_returnsImageOfTheSameSize() throws Exception {
+        // setup
+        FloatImage image = new FloatImage(300,200,3 );
+
+        // execute
+        image = filter.processImage(image);
+
+        // verify
+        assertThat(image.getWidth(), is (300));
+        assertThat(image.getHeight(), is (200));
+    }
+
+    @Test
+    public void givenAnyImage_returnsImageWithAtLeastThreeChannels() throws Exception {
+        // setup
+        FloatImage image = new FloatImage(0,0,1);
+
+        // execute
+        image = filter.processImage(image);
+
+        // verify
+        assertThat(image.getChannels(), is (greaterThan(2)));
+    }
+
+    @Test
+    public void givenOneRedPixelImage_returnsTransmuteColorPixelImage() throws Exception {
+        // setup
+        FloatImage image = new FloatImage(1,1,3);
+        image.setPixel(0,0,new float[]{1F,0F,0F});
+
+        // execute
+        image = filter.processImage(image);
+
+
+        // verify
+        float[] expected = {0F,1F,0F};
+        assertThat(Arrays.equals(image.getPixel(0,0), expected), is(true));
+    }
+
+    @Test
+    public void givenOneGreenPixelImage_returnsTransmuteColorPixelImage() throws Exception {
+        // setup
+        FloatImage image = new FloatImage(1,1,3);
+        image.setPixel(0,0,new float[]{0F,1F,0F});
+
+        // execute
+        image = filter.processImage(image);
+
+
+        // verify
+        float[] expected = {0F,0F,1F};
+        assertThat(Arrays.equals(image.getPixel(0,0), expected), is(true));
+    }
+
+    @Test
+    public void givenOneBluePixelImage_returnsTransmuteColorPixelImage() throws Exception {
+        // setup
+        FloatImage image = new FloatImage(1,1,3);
+        image.setPixel(0,0,new float[]{0F,0F,1F});
+
+        // execute
+        image = filter.processImage(image);
+
+
+        // verify
+        float[] expected = {1F,0F,0F};
+        assertThat(Arrays.equals(image.getPixel(0,0), expected), is(true));
+    }
+
+    @Test
+    public void givenOneYellowPixelImage_returnsDoubleTransmuteColorPixelImage() throws Exception {
+        // setup
+        FloatImage image = new FloatImage(1,1,3);
+        image.setPixel(0,0,new float[]{1F,1F,0F});
+
+        // execute
+        image = filter.processImage(image);
+
+
+        // verify
+        float[] expected = {0F,1F,1F};
+        assertThat(Arrays.equals(image.getPixel(0,0), expected), is(true));
+    }
+
+    @Test
+    public void givenMultiplePixelImage_returnsTransmuteColorImage() throws Exception {
+        // setup
+        FloatImage image = new FloatImage (2,2,3);
+        image.setPixel(0,0,new float[] {1F,0F,0F});
+        image.setPixel(1,0,new float[] {0F,1F,0F});
+        image.setPixel(0,1,new float[] {0F,0F,1F});
+        image.setPixel(1,1,new float[] {1F,1F,0F});
+
+        // execute
+        image= filter.processImage(image);
+
+        // verify
+        float [] expected = {
+        0F, 1F, 0F,
+        0F, 0F, 1F,
+        1F, 0F, 0F,
+        0F, 1F, 1F
+        };
+        System.out.println(Arrays.toString(image.getRawData()));
+        assertThat(Arrays.equals(image.getRawData(),expected), is(true));
+    }
+        // restore
+    }
+
+
+
+
