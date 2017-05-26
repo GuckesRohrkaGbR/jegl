b031ddc6ed8f16cf65e3447003d912d50c939c08
Christopher Guckes <christopher.guckes@torq-dev.de>
2017-05-26 18:20:09 +0200

    Refactored Prewitt filters

--- jegl-core/src/main/java/de/torqdev/jegl/filters/matrix/PrewittHorizontalEdgeDetectionFilter.java
+++ jegl-core/src/main/java/de/torqdev/jegl/filters/matrix/PrewittHorizontalEdgeDetectionFilter.java
@@ -13,7 +13,7 @@ import static java.lang.Math.*;
  * Created by jonas on 26.05.17.
  */
 @MetaInfServices
-public class PrewittHorizontalEdgeDetectionFilter implements ImageFilter {
+public class PrewittHorizontalEdgeDetectionFilter extends AbstractEdgeDetectorFilter {
     private static final float[] prewittOperator = new float[]{
             -1, -1, -1,
             0, 0, 0,
@@ -21,48 +21,7 @@ public class PrewittHorizontalEdgeDetectionFilter implements ImageFilter {
     };
     private static final float factor = 1F;
 
-    private ImageFilter grayscale = new AverageGrayscaleFilter();
-
-    @Override
-    public FloatImage processImage(FloatImage image) {
-        return applySobel(grayscale.processImage(image));
-    }
-
-    private FloatImage applySobel(FloatImage image) {
-        FloatImage prewittImage = new FloatImage(image.getWidth(), image.getHeight(),
-                image.getChannels());
-
-        IntStream.range(0, image.getHeight()).forEach(
-                y -> IntStream.range(0, image.getWidth()).forEach(
-                        x -> prewittImage.setPixel(x, y, calculatePixel(x, y, image))));
-
-        return prewittImage;
-    }
-
-    private float[] calculatePixel(int x, int y, FloatImage image) {
-        float[] newPixel = getArrayWithSameChannelsAs(x, y, image);
-        int channels = image.getChannels();
-
-        IntStream.range(-1, 2).forEach(matrixY -> IntStream.range(-1, 2).forEach(
-                matrixX -> IntStream.range(channels == 4 ? 1 : 0, channels).forEach(
-                        channel -> newPixel[channel] += image.getCappedPixel(x + matrixX,
-                                y + matrixY)
-                                [channel] * prewittOperator[(matrixX + 1) + (matrixY + 1) * 3])));
-
-        IntStream.range(channels == 4 ? 1 : 0, channels).forEach(
-                channel -> newPixel[channel] = normalize(newPixel[channel] *= factor));
-        return newPixel;
-    }
-
-    private float normalize(float value) {
-        return (max(-1F, min(1F, value)) + 1F) / 2F;
-    }
-
-    private float[] getArrayWithSameChannelsAs(int x, int y, FloatImage image) {
-        int channels = image.getChannels();
-        if (channels == 4) {
-            return new float[]{image.getPixel(x, y)[0], 0F, 0F, 0F};
-        }
-        return new float[channels];
+    protected PrewittHorizontalEdgeDetectionFilter() {
+        super(prewittOperator, factor);
     }
 }
--- jegl-core/src/main/java/de/torqdev/jegl/filters/matrix/PrewittVerticalEdgeDetectionFilter.java
+++ jegl-core/src/main/java/de/torqdev/jegl/filters/matrix/PrewittVerticalEdgeDetectionFilter.java
@@ -13,7 +13,7 @@ import static java.lang.Math.*;
  * Created by jonas on 26.05.17.
  */
 @MetaInfServices
-public class PrewittVerticalEdgeDetectionFilter implements ImageFilter {
+public class PrewittVerticalEdgeDetectionFilter extends AbstractEdgeDetectorFilter {
     private static final float[] prewittOperator = new float[]{
             -1, 0, 1,
             -1, 0, 1,
@@ -21,51 +21,9 @@ public class PrewittVerticalEdgeDetectionFilter implements ImageFilter {
     };
     private static final float factor = 1F;
 
-    private ImageFilter grayscale = new AverageGrayscaleFilter();
-
-    @Override
-    public FloatImage processImage(FloatImage image) {
-        return applyPrewitt(grayscale.processImage(image));
+    protected PrewittVerticalEdgeDetectionFilter() {
+        super(prewittOperator, factor);
     }
-
-    private FloatImage applyPrewitt(FloatImage image) {
-        FloatImage prewittImage = new FloatImage(image.getWidth(), image.getHeight(), image.getChannels());
-
-        IntStream.range(0, image.getHeight()).forEach(
-                y -> IntStream.range(0, image.getWidth()).forEach(
-                        x -> prewittImage.setPixel(x, y, calculatePixel(x, y, image))));
-
-        return prewittImage;
-    }
-
-    private float[] calculatePixel(int x, int y, FloatImage image) {
-        float[] newPixel = getArrayWithSameChannelsAs(x, y, image);
-        int channels = image.getChannels();
-
-        IntStream.range(-1, 2).forEach(matrixY -> IntStream.range(-1, 2).forEach(
-                matrixX -> IntStream.range(channels == 4 ? 1 : 0, channels).forEach(
-                        channel -> newPixel[channel] += image.getCappedPixel(x + matrixX,
-                                y + matrixY)
-                                [channel] * prewittOperator[(matrixX + 1) + (matrixY + 1) * 3])));
-
-        IntStream.range(channels == 4 ? 1 : 0, channels).forEach(
-                channel -> newPixel[channel] = normalize(newPixel[channel] *= factor));
-        return newPixel;
-
-    }
-
-    private float[] getArrayWithSameChannelsAs(int x, int y, FloatImage image) {
-        int channels = image.getChannels();
-        if (channels == 4) {
-            return new float[]{image.getPixel(x, y)[0], 0F, 0F, 0F};
-        }
-        return new float[channels];
-    }
-
-    private float normalize(float value) {
-        return (max(-1F, min(1F, value)) + 1F) / 2F;
-    }
-
 }
 
 
