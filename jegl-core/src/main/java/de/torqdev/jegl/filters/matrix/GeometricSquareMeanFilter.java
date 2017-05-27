6afc95861e26aa70aea3e3132d5063f55c21ee9b
Jonas Egert <jonas.egert@torq-dev.de>
2017-05-27 19:00:52 +0200

    Extracted GeometricSquareMeanFilter from SobelEdgeDetector.

---
+++ jegl-core/src/main/java/de/torqdev/jegl/filters/matrix/GeometricSquareMeanFilter.java
@@ -0,0 +1,61 @@
+package de.torqdev.jegl.filters.matrix;
+
+import de.torqdev.jegl.core.FloatImage;
+import de.torqdev.jegl.filters.ImageFilter;
+
+import java.util.stream.IntStream;
+
+import static java.lang.Math.*;
+
+/**
+ * @author <a href="mailto:jonas.egert@torq-dev.de">Jonas Egert</a>
+ * @version 1.0
+ */
+public class GeometricSquareMeanFilter implements ImageFilter {
+    private final ImageFilter filter1;
+    private final ImageFilter filter2;
+
+    public GeometricSquareMeanFilter(ImageFilter filter1, ImageFilter filter2) {
+        this.filter1 = filter1;
+        this.filter2 = filter2;
+    }
+
+    private FloatImage applyFilter(FloatImage image){
+        FloatImage image1 = filter1.processImage(image);
+        FloatImage image2 = filter2.processImage(image);
+
+        return combine(image1, image2);
+    }
+
+    private FloatImage combine(FloatImage image1, FloatImage image2) {
+        FloatImage filter = new FloatImage(image1.getWidth(), image1.getHeight(),1);
+
+        IntStream.range(0, filter.getHeight()).forEach(
+                y -> IntStream.range(0, filter.getWidth()).forEach(x -> filter.setPixel(x, y,
+                        combinePixels(
+                                image1.getPixel(
+                                        x,
+                                        y),
+                                image2.getPixel(
+                                        x,
+                                        y)))));
+        return filter;
+    }
+
+    private float[] combinePixels(float[] pixel1, float[] pixel2) {
+        float newPixel = (float) sqrt(
+                pixel1[0] * pixel1[0] + pixel2[0] * pixel2[0]);
+        newPixel = normalize(newPixel);
+        return new float[]{newPixel};
+    }
+
+    private float normalize(float newPixel) {
+        return max(0F, min(1F, newPixel));
+    }
+
+    @Override
+    public FloatImage processImage(FloatImage image) {
+        return applyFilter(image);
+    }
+}
+
--- jegl-core/src/main/java/de/torqdev/jegl/filters/matrix/SobelEdgeDetectionFilter.java
+++ jegl-core/src/main/java/de/torqdev/jegl/filters/matrix/SobelEdgeDetectionFilter.java
@@ -14,44 +14,14 @@ import static java.lang.Math.*;
  */
 @MetaInfServices
 public class SobelEdgeDetectionFilter implements ImageFilter {
-    private static final ImageFilter VERTICAL_SOBEL = new SobelVerticalEdgeDetectionFilter();
-    private static final ImageFilter HORIZONTAL_SOBEL = new SobelHorizontalEdgeDetectionFilter();
+    private static final ImageFilter combinationFilter =
+            new GeometricSquareMeanFilter(
+                    new SobelVerticalEdgeDetectionFilter(),
+                    new SobelHorizontalEdgeDetectionFilter()
+            );
 
     @Override
     public FloatImage processImage(FloatImage image) {
-        return applySobel(image);
-    }
-
-    private FloatImage applySobel(FloatImage image) {
-        FloatImage vertical = VERTICAL_SOBEL.processImage(image);
-        FloatImage horizontal = HORIZONTAL_SOBEL.processImage(image);
-
-        return combine(vertical, horizontal);
-    }
-
-    private FloatImage combine(FloatImage vertical, FloatImage horizontal) {
-        FloatImage sobel = new FloatImage(vertical.getWidth(), vertical.getHeight(), 1);
-
-        IntStream.range(0, sobel.getHeight()).forEach(
-                y -> IntStream.range(0, sobel.getWidth()).forEach(x -> sobel.setPixel(x, y,
-                                                                                      combinePixels(
-                                                                                              vertical.getPixel(
-                                                                                                      x,
-                                                                                                      y),
-                                                                                              horizontal.getPixel(
-                                                                                                      x,
-                                                                                                      y)))));
-        return sobel;
-    }
-
-    private float[] combinePixels(float[] verticalPixel, float[] horizontalPixel) {
-        float newPixel = (float) sqrt(
-                verticalPixel[0] * verticalPixel[0] + horizontalPixel[0] * horizontalPixel[0]);
-        newPixel = normalize(newPixel);
-        return new float[]{newPixel};
-    }
-
-    private float normalize(float newPixel) {
-        return max(0F, min(1F, newPixel));
+        return combinationFilter.processImage(image);
     }
 }
